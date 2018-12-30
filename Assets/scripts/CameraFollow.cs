882698c21955e76afb3a4fcde9b3cb76b03ba570
Uglydog777\Phil <uglydog777@gmail.com>
2018-12-29 18:57:39 -0600

    cam follow and flame

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    [SerializeField] float smoothSpeed = 0.125f;
+    [SerializeField] Vector3 cameraOffset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
--- Assets/scripts/PlayerMissile.cs
+++ Assets/scripts/PlayerMissile.cs
@@ -7,6 +7,11 @@ public class PlayerMissile : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    Vector2 startMissilePos;
+    Quaternion startMissileRot;
+
+    [SerializeField] SpriteRenderer missleFlame;
+
     [SerializeField] float missileThrust = 1f;
     [SerializeField] float missileRotSpeed = 1f;
 
@@ -14,13 +19,21 @@ public class PlayerMissile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();
+        startMissilePos = transform.position;
+        startMissileRot = transform.rotation;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerInput();
+
+        if(!Input.GetKey(KeyCode.W))
+        {
+            missleFlame.enabled = false;
+        }
     }
 
     private void PlayerInput()
@@ -31,7 +44,9 @@ public class PlayerMissile : MonoBehaviour
             {
                 print("should be moving");
                 rb.AddRelativeForce(Vector2.up *  missileThrust * Time.deltaTime);
+                missleFlame.enabled = true;
             }
+            
             if (!Input.GetKey(KeyCode.D))
             {
                 if (Input.GetKey(KeyCode.A))
@@ -49,6 +64,15 @@ public class PlayerMissile : MonoBehaviour
                     transform.Rotate(-Vector3.forward * missileRotSpeed);
                     rb.freezeRotation = false; //let physics take over again
                 }
+            }
+
+            // cheat stuff
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                rb.velocity = new Vector2(0f, 0f);
+                transform.position = startMissilePos;
+                transform.rotation = startMissileRot;
+
             }
         }
     }
